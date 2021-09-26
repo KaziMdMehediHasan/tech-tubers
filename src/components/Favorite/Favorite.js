6191ba5630994449e727927bb74d1fc45bf39473
= <=>
2021-09-26 13:13:15 +0600

    calculation added

--- src/components/Favorite/Favorite.js
+++ src/components/Favorite/Favorite.js
@@ -3,6 +3,10 @@ import "./Favorite.css";
 const Favorite = (props) => {
   //listing the favorite person
   console.log(props.favorite);
+
+  let sum = props.favorite.reduce((sum, curr) => {
+    return sum + curr.subscribers;
+  }, 0);
   const favorite = props.favorite.map((techie) => {
     return (
       <div className="mini-card">
@@ -15,6 +19,13 @@ const Favorite = (props) => {
     <div>
       <h3>Favorite List: {favorite.length}</h3>
       <div>{favorite}</div>
+      <hr />
+      <p className="sub-amount">
+        <i>
+          <b>Total Subscribers : </b>
+        </i>
+        <b>{sum.toFixed(2)}millions</b>
+      </p>
     </div>
   );
 };
